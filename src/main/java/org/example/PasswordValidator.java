c52cc679d3341dca2ff0fe5410885f6c5e005498
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-15 20:44:01 +0900

    testcode 리페토링 수정

--- src/main/java/org/example/PasswordValidator.java
+++ src/main/java/org/example/PasswordValidator.java
@@ -1,10 +1,12 @@
 package org.example;
 
 public class PasswordValidator {
+
+    private static final String WRONG_PASSWORD_LENGTH_EXCEPTIION_MESSAGE = "비밀 번호는 최소 8자 이상 1자 이하여야 한다.";
     public static void validate(String password) {
         int length = password.length();
         if(length < 8 || length > 12){
-            throw new IllegalArgumentException("비밀 번호는 최소 8자 이상 1자 이하여야 한다.");
+            throw new IllegalArgumentException(WRONG_PASSWORD_LENGTH_EXCEPTIION_MESSAGE);
         }
     }
 }
