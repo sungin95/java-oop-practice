be6382e388554dcc9e483c5bfe751052a37d0b79
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-15 18:46:28 +0900

    testcode 리펙토링 완성

--- src/main/java/org/example/PasswordValidator.java
+++ src/main/java/org/example/PasswordValidator.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class PasswordValidator {
     public static void validate(String password) {
-
+        int length = password.length();
+        if(length < 8 || length > 12){
+            throw new IllegalArgumentException("비밀 번호는 최소 8자 이상 1자 이하여야 한다.");
+        }
     }
 }
