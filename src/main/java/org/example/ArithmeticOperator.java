a6df23561bd2419e73888461950a1930c54888b7
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-17 15:54:49 +0900

    연산자 리펙토링

--- src/main/java/org/example/ArithmeticOperator.java
+++ src/main/java/org/example/ArithmeticOperator.java
@@ -33,11 +33,14 @@ public enum ArithmeticOperator {
 
     public abstract int arithmeticCalculate(final int operand1, final int operand2);
 
-    public static int c(int operand1, String operator, int operand2) {
+    public static int calculate(int operand1, String operator, int operand2) {
+
+
         ArithmeticOperator arithmeticOperator = Arrays.stream(values())
                 .filter(v -> v.operator.equals(operator))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
+
 
         return arithmeticOperator.arithmeticCalculate(operand1, operand2);
     }
--- src/main/java/org/example/PasswordGenerator.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-@FunctionalInterface
-public interface PasswordGenerator {
-    String generatePassword();
-}
