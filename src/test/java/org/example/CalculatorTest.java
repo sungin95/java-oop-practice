8b71ec911463e9823b4a8ff7d8a3448157539a11
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-17 11:53:46 +0900

    +-*/ 연산자 testcode parameterizedTest MethodSource 이용

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import org.assertj.core.util.Streams;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 
 /**
@@ -18,22 +25,23 @@ public class CalculatorTest {
     // 1 + 2 ----> Calculator
     // 3  <----
     @DisplayName("덧셈 연산을 수행한다.")
-    @Test
-    void additionTest() {
-        int result = Calculator.calculate(1, "+", 2);
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void calculateTest(int operand1, String operator, int operand2, int result) {
+        int calculateResult = Calculator.calculate(operand1, operator, operand2);
 
-        assertThat(result).isEqualTo(3);
-
-    }
-
-    // 1 + 2 ----> Calculator
-    // 3  <----
-    @DisplayName("뺄셈 연산을 수행한다.")
-    @Test
-    void subtractionTest() {
-        int result = Calculator.calculate(1, "-", 2);
-
-        assertThat(result).isEqualTo(-1);
+        assertThat(calculateResult).isEqualTo(result);
 
     }
+
+    // static은 뭔데 없으면 호출이 안돼?
+    private static Stream<Arguments> formulaAndResult() {
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(4, "*", 2, 8),
+                arguments(4, "/", 2, 2)
+        );
+    }
+
 }
