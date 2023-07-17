e5ad358910f98337ec0b72b11402e694e5d32d81
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-17 23:14:13 +0900

    학점 계산기 리펙토링

--- src/main/java/org/example/GradeCalculator.java
+++ src/main/java/org/example/GradeCalculator.java
@@ -3,10 +3,12 @@ package org.example;
 import java.util.List;
 
 public class GradeCalculator {
-    private final List<Course> courses;
+
+    private final Courses courses;
+
 
     public GradeCalculator(List<Course> courses) {
-        this.courses = courses;
+        this.courses = new Courses(courses);
     }
 
     /**
@@ -16,15 +18,10 @@ public class GradeCalculator {
      */
     public double calculateGrade() {
         // (학점수 X 교과목 평점)의 합계
-        double multipliedCreditAndCourseGrade = 0;
-        for (Course course : courses) {
-            multipliedCreditAndCourseGrade += course.multiplyCreditAndCouseGrade();
-        }
-
+        double multipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
         // 수강신청 총학점 수
-        int totalCompletedCredit = courses.stream()
-                .mapToInt(Course::getCredit)
-                .sum();
+        int totalCompletedCredit = courses.calculateTotalCompletedCredit();
+
         return multipliedCreditAndCourseGrade / totalCompletedCredit;
     }
 }
