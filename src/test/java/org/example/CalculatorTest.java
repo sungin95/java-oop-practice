7b6bd15f410685e37457ec2aa6fce51dfc2ad456
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-17 11:34:49 +0900

    덧셈 뺄셈 연산

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * 요구사항
+ * 간단한 사칙연산을 할 수 있다.
+ * 양수로만 계산할 수 있다.
+ * 나누셈에서 0을 나누는 경우 illegalArgument 예외를 발생시킨다.
+ * MVC패턴(Model-vew-Controller) 기반으로 구현한다.
+ */
+public class CalculatorTest {
+
+    // 1 + 2 ----> Calculator
+    // 3  <----
+    @DisplayName("덧셈 연산을 수행한다.")
+    @Test
+    void additionTest() {
+        int result = Calculator.calculate(1, "+", 2);
+
+        assertThat(result).isEqualTo(3);
+
+    }
+
+    // 1 + 2 ----> Calculator
+    // 3  <----
+    @DisplayName("뺄셈 연산을 수행한다.")
+    @Test
+    void subtractionTest() {
+        int result = Calculator.calculate(1, "-", 2);
+
+        assertThat(result).isEqualTo(-1);
+
+    }
+}
