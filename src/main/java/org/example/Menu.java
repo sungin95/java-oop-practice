ce96c5f879246895a9475f2c715098ba6c7fd2c1
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-18 17:07:43 +0900

    choose testcase

---
+++ src/main/java/org/example/Menu.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.List;
+
+public class Menu {
+
+    private final List<MenuItem> menuItems;
+
+    public Menu(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    public MenuItem choose(String name) {
+        return new MenuItem("돈까스", 5000);
+    }
+}
