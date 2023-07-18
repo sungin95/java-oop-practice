05f0bc8366cde2cff297e9596284f4e3b17e7ea1
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-18 15:18:48 +0900

    ì요리 주문 과정 ê공부

---
+++ src/main/java/org/example/Cook.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Cook {
+    private final String name;
+    private final int price;
+
+    public Cook(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
--- src/test/java/org/example/CookTest.java
+++ src/test/java/org/example/CookTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
-public class CourseTest {
-    @DisplayName("과목(코스)를 생성한다.")
+public class CookTest {
+
+    @DisplayName("요리를 생성한다.")
     @Test
     void createTest() {
-        assertThatCode(() -> new Course("OOP", 3, "A+"))
+        assertThatCode(() -> new Cook("만두", 5000))
                 .doesNotThrowAnyException();
     }
-
 }
